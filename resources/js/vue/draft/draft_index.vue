5d56fc48c271d3ac62ddf97e5a28feeba7e9affa
Ryan <ryan.haughee@gmail.com>
2022-05-06 17:17:21 -0400

    still trying to fix date

--- resources/js/vue/draft/draft_index.vue
+++ resources/js/vue/draft/draft_index.vue
@@ -152,8 +152,8 @@ export default {
             var sds = {};
             sds.league_id = self.league_id;
             $.get('/draft_function/get_otc_date', sds, function(response){
-                console.log(response.otc_time);
-                var new_otc_date = new Date(response.otc_time);
+                console.log(response.otc_time.replace(/-/g, "/"));
+                var new_otc_date = new Date(response.otc_time.replace(/-/g, "/"));
                 self.draft_date = new_otc_date;
                 self.current_pick_id = response.pick_id
             })
